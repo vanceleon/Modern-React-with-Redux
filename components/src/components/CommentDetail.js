260c5b76703fa4479347094f8a10c2606a7cc654
vanceleon <vanceleon44@gmail.com>
2019-01-08 21:33:38 -0500

    moved comment detail and moved faker import

--- components/src/components/CommentDetail.js
+++ components/src/components/CommentDetail.js
@@ -1,1 +1,23 @@
-import React from 'react';
+import React from "react";
+import faker from 'faker';
+
+const CommentDetail = (props) => {
+  return (
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img src={faker.image.avatar()} alt="avatar" />
+      </a>
+      <div className="content">
+        <a href="/" className="author">
+          Sam
+        </a>
+        <div className="metadata">
+          <span className="date">Today at 6:00pm</span>
+        </div>
+        <div className="text">Nice blog post!</div>
+      </div>
+    </div>
+  );
+};
+
+export default CommentDetail;
--- components/src/index.js
+++ components/src/index.js
@@ -1,27 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import faker from 'faker';
+import CommentDetail from './components/CommentDetail';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img src={faker.image.avatar()} alt="avatar" />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00pm</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
+      <CommentDetail/>
 
-      
+
+
     </div>
   );
 };
