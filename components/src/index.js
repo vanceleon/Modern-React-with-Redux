1f04ed4871398247e64baae10edabe563aa01a37
vanceleon <vanceleon44@gmail.com>
2019-01-09 20:57:33 -0500

    wrapped approval card component around comment

--- components/src/index.js
+++ components/src/index.js
@@ -2,19 +2,22 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./components/CommentDetail";
 import ApprovalCard from "./components/ApprovalCard";
+import fakerStatic from 'faker';
+
+
 
 class App extends Component {
   state = {
     author: "Vance",
-    time: "Today at 6:00pm",
+    time: "Today at 6:00pm"
   };
 
   render() {
     return (
       <div className="ui container comments">
-          <ApprovalCard/>
-        <CommentDetail author={this.state.author} time={this.state.time} />
-
+        <ApprovalCard>
+          <CommentDetail author={this.state.author} time={this.state.time} avatar={fakerStatic.image.avatar()}/>
+        </ApprovalCard>
       </div>
     );
   }
